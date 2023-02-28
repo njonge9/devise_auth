c6e4854aa263c6a2c3ca217745811167a5381c30
njonge9 <frednjonge9@gmail.com>
2023-02-28 03:53:36 -0800

    Finishing the devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
-    skip_before_action :authenticate_user!, only:i%[homepage]
+    skip_before_action :authenticate_user!, only: %i[homepage]
+
     def homepage
 
     end
